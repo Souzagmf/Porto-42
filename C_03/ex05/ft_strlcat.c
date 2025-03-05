594c6551be4bb335680b2a80537a7dbb42377ccb
Ubuntu <ubuntu@ip-172-31-11-48.eu-west-3.compute.internal>
2025-03-05 00:22:04 +0000

    adding ex05

--- C_03/ex05/ft_strlcat.c
+++ C_03/ex05/ft_strlcat.c
@@ -2,64 +2,50 @@
 #include <string.h>
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size);
-int ft_count(char *str);
 
 int main(void)
 {
     char arr_buffer[50] = "She's so beautiful || ";
-    char arr_str[] = "And I tell her every day";
-    char arr_buffer2[50] = "She's so beautiful || ";
-    char arr_str2[] = "And I tell her every day";
-    unsigned int result;
-    unsigned int result_original;
+    char arr_str[] = "And I tell her every day, cause your amazing, just the way you are!";
+    char arr_buffer_2[50] = "She's so beautiful || ";
+    char arr_str_2[] = "And I tell her every day, cause your amazing, just the way you are!";
+    unsigned int result, result_2;
 
-    result_original = strlcat(arr_buffer2, arr_str2,23);
-    result = ft_strlcat(arr_buffer,arr_str, 23);
-    printf("%s\nOriginal: %s\n", arr_buffer, arr_buffer2);
-    printf("%u\nOriginal: %u", result, result_original);
+    result_2 = strlcat(arr_buffer_2, arr_str_2, sizeof(arr_buffer_2));
+    result = ft_strlcat(arr_buffer,arr_str, sizeof(arr_buffer));
+    printf("Phrase My: %s\n", arr_buffer);
+    printf("Return My: %u\n", result);
+    printf("Phrase Of: %s\n", arr_buffer_2);
+    printf("Return Of: %u\n", result_2);
 }
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {   
-    unsigned int src_size;
-    unsigned int calc_size;
-    unsigned int dest_size;
+	unsigned int	src_size;
+	unsigned int	dest_size;
+	unsigned int	index;
 
-    src_size = ft_count(src);
-    dest_size = 0;
-
-    while (dest[dest_size] && dest_size < size) //set the size of (dest)
-    {
-        dest_size++;
-    }
-
-    if(dest_size >= size) //Case dest dont have space to concatenate
-    {
-        return size + src_size;
-    }
-
-    calc_size = size - dest_size - 1; // -1 to '\0'
-
-    while (calc_size > 0)
-    {
-            dest[dest_size++] = *src++; //use the data and after increment
-            calc_size--;
-    }
-
-    dest[dest_size] = '\0';
-
-    return (dest_size + src_size);
+	src_size = 0;
+	dest_size = 0;
+	index = 0;
+	while (dest[dest_size] && dest_size < size) //set the size of (dest)
+	{
+		dest_size++;
+	}
+	while (src[src_size])
+	{
+		src_size++;	
+	}
+	if(dest_size >= size - 1) //Case dest dont have space to concatenate
+	{
+		return size + src_size;
+	}
+	while (index < src_size && (dest_size + index + 1) < size)
+	{
+		dest[dest_size + index] = src[index]; //use the data and after increment
+		index++;
+	}
+	dest[dest_size + index] = '\0';
+	return (dest_size + src_size);
     
-}
-
-int ft_count(char *str)
-{
-    int count;
-
-    count = 0;
-    while (*str++)
-    {
-        count++;
-    }
-    return (count);
 }
